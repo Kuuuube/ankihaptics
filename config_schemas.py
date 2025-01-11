34c465acd38595179e1568c740268231ed3e3cd3
kuuuube <hexagonisalie@gmail.com>
2025-01-11 11:56:24 -0500

    Add show question and show answer hooks

--- config_schemas.py
+++ config_schemas.py
@@ -61,6 +61,30 @@ easy_button_schema = {
     }
 }
 
+show_question_schema = {
+    "enabled": {
+        "default": False
+    },
+    "strength": {
+        "default": 0.1
+    },
+    "duration": {
+        "default": 1.0
+    }
+}
+
+show_answer_schema = {
+    "enabled": {
+        "default": False
+    },
+    "strength": {
+        "default": 0.1
+    },
+    "duration": {
+        "default": 1.0
+    }
+}
+
 device_schema = {
     "device_name": {
         "default": ""
@@ -82,5 +106,11 @@ device_schema = {
     },
     "easy": {
         "default": easy_button_schema
+    },
+    "show_question": {
+        "default": show_question_schema
+    },
+    "show_answer": {
+        "default": show_answer_schema
     }
 }
--- hooks.py
+++ hooks.py
@@ -8,20 +8,23 @@ ease_to_button = {
     4: "easy"
 }
 
-def answer_button_press(mw, client, reviewer, card, ease):
+def handle_hooks(mw, client, hook):
     config = config_util.get_config(mw)
     for device in config["devices"]:
-        button_name = ease_to_button[ease]
-        if device[button_name]["enabled"]:
-            print("Activating device. Strength: " + str(device[button_name]["strength"]) + ", Duration: " + str(device[button_name]["duration"]) + "s")
+        if device[hook]["enabled"]:
+            print("Activating device. Strength: " + str(device[hook]["strength"]) + ", Duration: " + str(device[hook]["duration"]) + "s")
 
-    # for device in client.devices:
-    #     if len(device.actuators) != 0:
-    #         await device.actuators[0].command(0.5)
-    #     if len(device.linear_actuators) != 0:
-    #         await device.linear_actuators[0].command(1000, 0.5)
-    #     if len(device.rotatory_actuators) != 0:
-    #         await device.rotatory_actuators[0].command(0.5, True)
+def answer_button_press(mw, client, reviewer, card, ease):
+    button_name = ease_to_button[ease]
+    handle_hooks(mw, client, button_name)
+
+def show_question(mw, client, card):
+    handle_hooks(mw, client, "show_question")
+
+def show_answer(mw, client, card):
+    handle_hooks(mw, client, "show_answer")
 
 def register_hooks(mw, client):
     gui_hooks.reviewer_did_answer_card.append(lambda reviewer, card, ease: answer_button_press(mw, client, reviewer, card, ease))
+    gui_hooks.reviewer_did_show_question.append(lambda card: show_question(mw, client, card))
+    gui_hooks.reviewer_did_show_answer.append(lambda card: show_answer(mw, client, card))

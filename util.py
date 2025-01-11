be099402a55c663e4ddd4d08ef2d4292026ea766
kuuuube <hexagonisalie@gmail.com>
2025-01-11 18:50:04 -0500

    Format util

--- util.py
+++ util.py
@@ -1,11 +1,13 @@
 import asyncio
+from collections.abc import Callable
 
-def try_parse_float(input_string):
+
+def try_parse_float(input_string: str) -> float:
     try:
         return float(input_string)
-    except Exception:
+    except Exception:  # noqa: BLE001
         return None
 
 #Allows running an async function on a thread
-def start_async(function):
+def start_async(function: Callable) -> None:
     asyncio.run(function())

53e454b13e2771ec60acd7239960840a6801ff29
kuuuube <hexagonisalie@gmail.com>
2025-01-10 21:19:05 -0500

    Add function for starting async on thread

--- util.py
+++ util.py
@@ -1,5 +1,11 @@
+import asyncio
+
 def try_parse_float(input_string):
     try:
         return float(input_string)
     except Exception:
         return None
+
+#Allows running an async function on a thread
+def start_async(function):
+    asyncio.run(function())

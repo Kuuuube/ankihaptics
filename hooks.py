ef457d0923332d185c73e01c829a3409c778dc14
kuuuube <hexagonisalie@gmail.com>
2025-01-11 19:13:04 -0500

    Partially type and format hooks

--- hooks.py
+++ hooks.py
@@ -1,14 +1,16 @@
-from aqt import gui_hooks
+from aqt import gui_hooks, main, reviewer
+from buttplug import Client
+
 from . import config_util, haptics_commands
 
 ease_to_button = {
     1: "again",
     2: "hard",
     3: "good",
-    4: "easy"
+    4: "easy",
 }
 
-def handle_hooks(mw, client, hook):
+def _handle_hooks(mw: main.AnkiQt, client: Client, hook: str) -> None:
     config = config_util.ensure_device_settings(config_util.get_config(mw), client.devices)
     devices = client.devices
     for config_device in config["devices"]:
@@ -21,18 +23,18 @@ def handle_hooks(mw, client, hook):
                     for target_actuator in target_actuators:
                         haptics_commands.run_scalar_command(target_actuator, int(config_device[hook]["strength"] * (target_actuator.step_count / 99)), 0, config_device[hook]["duration"])
 
-def answer_button_press(mw, client, reviewer, card, ease):
+def _answer_button_press(mw: main.AnkiQt, client: Client, _reviewer: reviewer.Reviewer, _card, ease: int) -> None:
     button_name = ease_to_button[ease]
-    handle_hooks(mw, client, button_name)
+    _handle_hooks(mw, client, button_name)
 
-def show_question(mw, client, card):
-    handle_hooks(mw, client, "show_question")
+def _show_question(mw: main.AnkiQt, client: Client, _card) -> None:
+    _handle_hooks(mw, client, "show_question")
 
-def show_answer(mw, client, card):
-    handle_hooks(mw, client, "show_answer")
+def _show_answer(mw: main.AnkiQt, client: Client, _card) -> None:
+    _handle_hooks(mw, client, "show_answer")
 
-def register_hooks(mw, client):
+def register_hooks(mw: main.AnkiQt, client: Client) -> None:
     # https://github.com/ankitects/anki/blob/main/qt/tools/genhooks_gui.py
-    gui_hooks.reviewer_did_answer_card.append(lambda reviewer, card, ease: answer_button_press(mw, client, reviewer, card, ease))
-    gui_hooks.reviewer_did_show_question.append(lambda card: show_question(mw, client, card))
-    gui_hooks.reviewer_did_show_answer.append(lambda card: show_answer(mw, client, card))
+    gui_hooks.reviewer_did_answer_card.append(lambda reviewer, card, ease: _answer_button_press(mw, client, reviewer, card, ease))
+    gui_hooks.reviewer_did_show_question.append(lambda card: _show_question(mw, client, card))
+    gui_hooks.reviewer_did_show_answer.append(lambda card: _show_answer(mw, client, card))

2f23165c372db6703e072a3c664bceaa2377228d
kuuuube <hexagonisalie@gmail.com>
2025-01-12 11:38:33 -0500

    Add actuator type annotations

--- haptics_commands.py
+++ haptics_commands.py
@@ -1,13 +1,15 @@
 import asyncio
 import threading
 
+import buttplug
+
 from . import util
 
 
-def run_scalar_command(actuator, start_strength: float, end_strength: float, duration: float) -> None:
+def run_scalar_command(actuator: buttplug.client.Actuator, start_strength: float, end_strength: float, duration: float) -> None:
     threading.Thread(target = lambda: util.start_async(lambda: scalar_command(actuator, start_strength, end_strength, duration)))
 
-async def scalar_command(actuator, start_strength: float, end_strength: float, duration: float) -> None:
+async def scalar_command(actuator: buttplug.client.Actuator, start_strength: float, end_strength: float, duration: float) -> None:
     await actuator.command(start_strength)
     await asyncio.sleep(duration)
     await actuator.command(end_strength)

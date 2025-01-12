f26ff78438386f677056b8db3a791edb5b70a805
kuuuube <hexagonisalie@gmail.com>
2025-01-12 17:53:16 -0500

    Cut down nasty type checking issues in hooks

--- hooks.py
+++ hooks.py
@@ -1,6 +1,5 @@
 import anki
 import aqt.reviewer
-import buttplug
 
 from . import config_util
 
@@ -11,28 +10,28 @@ ease_to_button = {
     4: "easy",
 }
 
-def _handle_hooks(mw: aqt.main.AnkiQt, ankihaptics_ref, hook: str) -> None:
+def _handle_hooks(mw: aqt.main.AnkiQt, ankihaptics_ref, hook: str) -> None:  # noqa: ANN001
     config = config_util.ensure_device_settings(config_util.get_config(mw), ankihaptics_ref.client.devices)
-    devices: buttplug.Client.devices = ankihaptics_ref.client.devices
+    devices = ankihaptics_ref.client.devices
     websocket_command = {"command": "scalar_cmd", "args": {"devices": []}}
     for config_device in config["devices"]:
         if config_device["enabled"] and config_device[hook]["enabled"]:
-            client_device: buttplug.Device = [device for device in devices.values() if device.name == config_device["device_name"]][0] #should only return one device
+            client_device = [device for device in devices.values() if device.name == config_device["device_name"]][0] #should only return one device
             websocket_command["args"]["devices"].append({"index": client_device.index, "actuators": client_device.actuators, "strength": config_device[hook]["strength"]})
             websocket_command["args"]["duration"] = config["duration"][hook]
             ankihaptics_ref.websocket_command = websocket_command
 
-def _answer_button_press(mw: aqt.main.AnkiQt, ankihaptics_ref, _reviewer: aqt.reviewer.Reviewer, _card: anki.cards.Card, ease: int) -> None:
+def _answer_button_press(mw: aqt.main.AnkiQt, ankihaptics_ref, _reviewer: aqt.reviewer.Reviewer, _card: anki.cards.Card, ease: int) -> None:  # noqa: ANN001
     button_name = ease_to_button[ease]
     _handle_hooks(mw, ankihaptics_ref, button_name)
 
-def _show_question(mw: aqt.main.AnkiQt, ankihaptics_ref, _card: anki.cards.Card) -> None:
+def _show_question(mw: aqt.main.AnkiQt, ankihaptics_ref, _card: anki.cards.Card) -> None:  # noqa: ANN001
     _handle_hooks(mw, ankihaptics_ref, "show_question")
 
-def _show_answer(mw: aqt.main.AnkiQt, ankihaptics_ref, _card: anki.cards.Card) -> None:
+def _show_answer(mw: aqt.main.AnkiQt, ankihaptics_ref, _card: anki.cards.Card) -> None:  # noqa: ANN001
     _handle_hooks(mw, ankihaptics_ref, "show_answer")
 
-def register_hooks(mw: aqt.main.AnkiQt, ankihaptics_ref) -> None:
+def register_hooks(mw: aqt.main.AnkiQt, ankihaptics_ref) -> None:  # noqa: ANN001
     # https://github.com/ankitects/anki/blob/main/qt/tools/genhooks_gui.py
     aqt.gui_hooks.reviewer_did_answer_card.append(lambda reviewer, card, ease: _answer_button_press(mw, ankihaptics_ref, reviewer, card, ease))
     aqt.gui_hooks.reviewer_did_show_question.append(lambda card: _show_question(mw, ankihaptics_ref, card))
